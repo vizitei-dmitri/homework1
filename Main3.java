6725771639822d2a41be1c9909b8ff5bc929965f
Otkisaev <109982826+vizitei-dmitri@users.noreply.github.com>
2022-07-29 21:40:44 +0300

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static int mmm (int a, int b) {
+        return (int) (a * b - 3);
+    }
+    public static void main(String ...args){
+        int o = mmm(10,5);
+        System.out.println("o = " + o);
+    }
+
+
+}
