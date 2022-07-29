ef62b62717e27e4eb3582d44d9ade10a8fa8b99d
Otkisaev <109982826+vizitei-dmitri@users.noreply.github.com>
2022-07-29 21:39:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String ...arg){
+        double a = 12.1212;
+        int b = 22;
+        float c = (float) 9.199;
+        short d = (short) 5.1;
+        int x = (int) (a*b + c*d);
+        System.out.println("число = " + x);
+    }
+}
